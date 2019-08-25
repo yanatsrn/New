0caf4ec652b2cdf0993b7f90f84b4b07a0e27b87
yanatsrn <54328632+yanatsrn@users.noreply.github.com>
2019-08-25 22:28:50 +0300

    Arifmetika

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int kek = 5;
+        int lol = kek + 2;
+        double piy = (double) lol / kek;
+        System.out.println(" Сумма = " + (kek + lol));
+        System.out.println("Разность = " + (kek - lol));
+        System.out.println("Произведение = " + (kek * lol));
+        System.out.println("Частное = " + (lol / kek));
+        System.out.println("Дробное частное = " + piy);
+    }
+}
